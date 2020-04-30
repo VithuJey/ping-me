8d60edfe9c7de11d835ff9a4d1ffd3813ba94467
vithushan_IT16041776 <jeyasothy.vithushan@my.sliit.lk>
2020-04-30 18:47:00 +0530

    Add day-time changes

--- lib/pages/day_time.dart
+++ lib/pages/day_time.dart
@@ -7,7 +7,8 @@ class DayTimePage extends StatefulWidget {
 
 class _DayTimeState extends State<DayTimePage> {
 
-  Map<String, TimeOfDay> _day_time = new Map<String, TimeOfDay>();
+  List dayTime = [];
+  // {"day":"Monday", "time":TimeOfDay(hour: 12,minute:00)}
 
   List<String> days = ["Select a day", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
   String _day;
@@ -20,9 +21,21 @@ class _DayTimeState extends State<DayTimePage> {
     _time = TimeOfDay(hour: 00,minute:00);
   }
 
+  _popDataBack() async {
+
+    Navigator.pop(context, dayTime);
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    this.dayTime = this.dayTime.isEmpty ? ModalRoute.of(context).settings.arguments : this.dayTime;
+
+    if(dayTime.isNotEmpty) {
+      for(int i=0;i<dayTime.length;i++) {
+        days.remove(dayTime[i]["day"]);
+      }
+    }
     
     return Scaffold(
       appBar: AppBar(
@@ -31,63 +44,74 @@ class _DayTimeState extends State<DayTimePage> {
 
       ),
       body:Padding(
-            padding: const EdgeInsets.all(20.0),
-            child:Column(
-              children: <Widget>[
+        padding: const EdgeInsets.all(20.0),
+        child:Column(
+          children: <Widget>[
 
-                // Day_Dropdown
-                DropdownButton<String>(
-                  isExpanded: true,
-                  items : days.map((String value){
-                    return DropdownMenuItem<String>(
-                    value :value,
-                    child: Text (value)
-                    );
-                  }).toList(),
-                  value: this._day,
-                  onChanged:(String value) {
-                      setState(() {
-                        this._day = value;
-                      });
-                  }
-                ),
-                  
-                // Time_Picker
-                ListTile(
-                  title: (_time==TimeOfDay(hour: 00,minute:00))?Text("Select a time"):Text("${_time.hour}:${_time.minute}"),
-                  trailing: Icon(Icons.keyboard_arrow_down),
-                  onTap: _pickTime,
-                ),
+            // Day_Dropdown
+            DropdownButton<String>(
+              isExpanded: true,
+              items : days.map((String value){
+                return DropdownMenuItem<String>(
+                value :value,
+                child: Text (value)
+                );
+              }).toList(),
+              value: this._day,
+              onChanged:(String value) {
+                  setState(() {
+                    this._day = value;
+                  });
+              }
+            ),
+              
+            // Time_Picker
+            ListTile(
+              title: (_time==TimeOfDay(hour: 00,minute:00))?Text("Select a time"):Text("${_time.hour}:${_time.minute}"),
+              trailing: Icon(Icons.keyboard_arrow_down),
+              onTap: _pickTime,
+            ),
 
-                // Add Button
-                RaisedButton(onPressed: _addDayTime, child: Text("Add")),
+            // Add Button
+            // RaisedButton(onPressed: _clearAll, child: Text("Clear All")),
+            RaisedButton(onPressed: _addDayTime, child: Text("Add")),
 
-              ],
-            )
-      )
-      
+            // List of Day-Time
+            dayTime.isNotEmpty ? Text("${dayTime.length} record added...") : Text("No day and time selected...")
+
+          ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: _popDataBack, tooltip: 'Save', child: Icon(Icons.save),),
     );
   }
 
   _pickTime() async {
     TimeOfDay t = await showTimePicker(
-        context: context,
-        initialTime: _time
-      );    if(t != null)
-        setState(() {
-          _time = t;
-        });  
+      context: context,
+      initialTime: _time
+    );    
+    if(t != null)
+    setState(() {
+      _time = t;
+    });
   }
 
-  _addDayTime() async {
-      _day_time.putIfAbsent(_day, () => _time);      
-      days.remove(_day);
-      setState(() {
-        _day = "Select a day";
-        _time=TimeOfDay(hour: 00,minute:00);
-      });
-      print(days);
-      print(_day_time);
+  _addDayTime() {
+    this.dayTime.add({"day":this._day, "time":this._time});     
+    days.remove(_day);
+    setState(() {
+      this._day = "Select a day";
+      this._time=TimeOfDay(hour: 00,minute:00);
+      this.dayTime = dayTime;
+    });
+  }
+
+  _clearAll() {
+    this.dayTime.clear();
+    setState(() {
+      this.dayTime = [];
+    });
   }
   
 }
--- lib/pages/details_list.dart
+++ lib/pages/details_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:contact_picker/contact_picker.dart';
 
 class DetailsListPage extends StatefulWidget {
   // DetailsListPage({Key key, this.title}) : super(key: key);
@@ -11,35 +12,77 @@ class DetailsListPage extends StatefulWidget {
 
 class _DetailsListState extends State<DetailsListPage> {
 
+  final ContactPicker _contactPicker = new ContactPicker();
+  Contact _contact;
+  
+  List dayTime = [];
+
   @override
   Widget build(BuildContext context) {
+
+    // this.dayTime = this.dayTime.isEmpty ? ModalRoute.of(context).settings.arguments : this.dayTime;
+    print(dayTime);
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        // title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        padding: EdgeInsets.all(15.0),
         child: Column(
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-
-          ],
+          children: <Widget>[Flexible(child: dayTimeList())],
         ),
       ),
+      
       floatingActionButton: FloatingActionButton(
         // onPressed: _incrementCounter,
-        onPressed: () {
-          Navigator.pushNamed(context, '/day_time');
-        },
+        onPressed: () async {
+                  Contact contact = await _contactPicker.selectContact();
+                  setState(() {
+                    _contact = contact;
+                    print(_contact);
+                  });
+                },
+        
+        // () async {
+        //   dynamic result = await Navigator.pushNamed(context, '/day_time', arguments: dayTime);
+        //   setState(() {
+        //     dayTime = (result == null ? [] : result);
+        //   });
+        // },
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
+
+  Widget dayTimeList() {
+  return SingleChildScrollView(
+    child: Card(
+      child: ExpansionTile(
+        title: Text(
+          "Day - Time",
+          style: new TextStyle(),
+          textAlign: TextAlign.center,
+        ),
+        children: ListMyWidgets(),
+      ),
+    ),
+  );
+}
+
+  List<Widget> ListMyWidgets() {
+    List<Widget> list = new List();
+    for(int index=0;index<dayTime.length;index++) {
+      list.add( 
+        ListTile(
+            title: Text("${dayTime[index]["day"]} - ${dayTime[index]["time"].hour}:${dayTime[index]["time"].minute} "),
+            trailing: Icon(Icons.more_vert),
+          ),
+        
+      );
+    }
+    return list;
+  }
+
 }
 
